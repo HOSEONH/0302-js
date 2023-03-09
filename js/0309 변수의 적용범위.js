c15b81f7c29aef27a96cdf6235834af15cecaf08
HS H <3262398@naver.com>
2023-03-09 18:15:10 +0900

    0309 수업 마무리

---
+++ js/0309 변수의 적용범위.js
@@ -0,0 +1,8 @@
+let name = "홍길동";
+document.write(`<p>${name}</p>`);
+document.write(`<p>${name}</p>`);
+
+var bookTitle = "책이름";
+document.write(`<p>${bookTitle}</p>`);
+// 다른 사람들과 협업하면서 변수 이름을 썼다는 것을 알 수 없게 되고,
+// 똑같이 사용할 경우 
